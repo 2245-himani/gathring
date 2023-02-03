3efcee5a13dbed04e23abc549ab67bd4718ca22d
2245-himani <123562229+2245-himani@users.noreply.github.com>
2023-02-03 19:21:52 +0530

    Add files via upload

---
+++ gathring-1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void ave (int num[])
+{
+	int i,sum=0,avrage;
+	for(i=0;i<=4;i++)
+	{
+		sum=sum+num[i];
+	}
+	printf("sum of : %d",sum);
+}
+main()
+{
+	{
+	
+	int a[]={1,2,3,4,5};
+	sum (a);
+}
+}
